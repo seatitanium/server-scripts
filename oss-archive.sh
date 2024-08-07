5867f042b3d53c301493233c9652efa9f694c49e
Subilan <suyuanliang1995@gmail.com>
2024-08-07 22:29:58 +0800

    fix: not just archive world

--- oss-archive.sh
+++ oss-archive.sh
@@ -1,5 +1,5 @@
 ossdir=~/oss
 archive_remote_dir=oss://seatibackup/archive/
-archive_local_dir=/mc/server/world
+archive_local_dir=/mc/server
 $ossdir/oss rmrf $archive_remote_dir
 $ossdir/oss cp $archive_local_dir $archive_remote_dir
